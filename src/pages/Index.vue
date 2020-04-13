1986f009f9e6930d762d45a0de017fecfcd6f1df
Gabriel Rodrigues <romulogabriel@outlook.com>
2020-04-12 21:11:01 -0300

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+    <p class="text-body1">
+      Hi I'm being served on Netlify
+    </p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
